15750084634a8fa5f14410ae466a67996b36612b
Soporte B <datacenter.escuela@gmail.com>
2023-06-27 12:19:38 -0500

    DB-vistas

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -2,7 +2,15 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
+
 urlpatterns = [
-    path("", views.index, name="index"),
-    
+    #es; /polls/
+    path("", views.IndexView.as_view(), name="index"),
+    #es; /polls/5/
+    path("<int:pk>/detail/messi", views.DetailView.as_view(), name="detail"),
+    #es; /polls/5/results/
+    path("<int:pk>/results/", views.ResultView.as_view(), name="results"),
+    #es; /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
