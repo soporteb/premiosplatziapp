a93071974e06ee80c74af70fb9bf8885e343b4eb
Soporte B <datacenter.escuela@gmail.com>
2023-06-28 15:28:53 -0500

    Configurando el Admin y añadiendo funcionalidades por el admin.py

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import Question 
+from .models import Choice, Question 
 
-admin.site.register(Question)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 1
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
 # Register your models here.
